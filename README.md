2a5542a05745a09cf7fb9d43f4e01e3ef61d6f2f
bfudeus <benjamin@fudeus.net>
2026-04-19 17:21:10 +0200

    Added new vegetation zone

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 A game similar to POKEMON where you must find creatures called
 monpoks and collect them.
 
-Worktime: 5h200min
+Worktime: 5h00min
 
-v.1.7
+v.1.8
--- script/music_change_by_area.gd
+++ script/music_change_by_area.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
+@export var music: AudioStream
 @onready var audio_stream_player = get_tree().root.find_child("AudioStreamPlayer2D", true, false)
-@onready var stream = preload("res://Assets/sounds/EndlessTallGrass.mp3")
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		audio_stream_player.stream = stream
+		audio_stream_player.stream = music
 		audio_stream_player.play()
